359e3442663788f3ea2455854b587bab69902b95
gabriel.santos <gabrieldossantossilva2108@gmail.com>
2023-10-21 18:46:22 -0300

    FEAT: termino acionamento

--- lib/datasource/remote/veterinarian_remote.dart
+++ lib/datasource/remote/veterinarian_remote.dart
@@ -4,19 +4,30 @@ import 'package:enagro_app/models/veterinarian.dart';
 import 'package:enagro_app/helpers/Util.dart';
 import 'package:enagro_app/infra/general_http_client.dart';
 
-
 class VeterinarianRemote {
   final url = Util.concatenateEndpoint("veterinarian/");
 
   Future<Veterinarian> getByUser(int userId) async {
     var data = await GeneralHttpClient().getJson('${url}getByUser/$userId');
-    Veterinarian vet = Veterinarian.fromMap(data['dados'].length > 0 ? data['dados'][0] : {});
+    Veterinarian vet =
+        Veterinarian.fromMap(data['dados'].length > 0 ? data['dados'][0] : {});
     return vet;
   }
 
-   Future<Veterinarian> saveVeterinarian(Object prms) async {
+  Future<List<Veterinarian>> getByServicesAndCities(
+      String servicesId, String animalsId) async {
+    var data = await GeneralHttpClient()
+        .getJson('${url}getByServicesAndCities/$servicesId/$animalsId');
+    List<Veterinarian> vet = [];
+    if (data['dados'].length > 0) {
+      vet = Veterinarian.getVeterinarians(data['dados']);
+    }
+    return vet;
+  }
+
+  Future<Veterinarian> saveVeterinarian(Object prms) async {
     var data = await GeneralHttpClient().post('${url}store', jsonEncode(prms));
-    if(data['success']) {
+    if (data['success']) {
       Veterinarian vet = Veterinarian.fromMap(data['dados']);
       return vet;
     }
