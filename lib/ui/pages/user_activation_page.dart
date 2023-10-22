7015403bed1aa3247965f7a66504b3553916830d
gabriel.santos <gabrieldossantossilva2108@gmail.com>
2023-10-22 17:05:48 -0300

    FEAT: termino modal veterinario

--- lib/ui/pages/user_activation_page.dart
+++ lib/ui/pages/user_activation_page.dart
@@ -4,6 +4,7 @@ import 'package:enagro_app/datasource/remote/activation_remote.dart';
 import 'package:enagro_app/models/activation.dart';
 import 'package:enagro_app/models/user.dart';
 import 'package:enagro_app/ui/widgets/activation_card.dart';
+import 'package:enagro_app/ui/widgets/default_outline_button.dart';
 import 'package:flutter/material.dart';
 
 class UserActivationPage extends StatefulWidget {
@@ -15,6 +16,66 @@ class UserActivationPage extends StatefulWidget {
 }
 
 class _UserActivationPageState extends State<UserActivationPage> {
+  void _showServicesModal() {
+    showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return Container(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            children: [
+              Text("Lista de Serviços"),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  void _showAnimalsModal() {
+    showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return Container(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            children: [
+              Text("Lista de Animais"),
+              // Aqui você pode adicionar a lista de animais
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  void _showVeterinarianModal(String veterinarianName) {
+    showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return Container(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            children: [
+              const Spacer(),
+              Text("Veterinário: $veterinarianName.",
+                  style: const TextStyle(
+                      fontSize: 20, fontWeight: FontWeight.bold)),
+              const Spacer(),
+              DefaultOutlineButton(
+                'Fechar',
+                () {
+                  Navigator.pop(context);
+                },
+                style: TextStyle(color: Theme.of(context).primaryColor),
+              )
+            ],
+          ),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -47,63 +108,64 @@ class _UserActivationPageState extends State<UserActivationPage> {
                       itemBuilder: (context, index) {
                         Activation activation = snapshot.data![index];
                         return Container(
-                          padding: const EdgeInsets.all(20),
-                          // margin: const EdgeInsets.only(bottom: 10, top: 10),
+                            padding: const EdgeInsets.all(20),
+                            // margin: const EdgeInsets.only(bottom: 10, top: 10),
                             child: CardActions(
-                          showToolTip: true,
-                          backgroundColor: Theme.of(context).primaryColor,
-                          axisDirection: CardActionAxis.bottom,
-                          borderRadius: 15,
-                          width: MediaQuery.of(context).size.width * 0.98,
-                          height: MediaQuery.of(context).size.height * 0.25,
-                          actions: <CardActionButton>[
-                            CardActionButton(
-                              icon: const Icon(
-                                Icons.person,
-                                color: Colors.white,
-                                size: 20,
+                              showToolTip: true,
+                              backgroundColor: Theme.of(context).primaryColor,
+                              axisDirection: CardActionAxis.bottom,
+                              borderRadius: 15,
+                              width: MediaQuery.of(context).size.width * 0.98,
+                              height: MediaQuery.of(context).size.height * 0.25,
+                              actions: <CardActionButton>[
+                                CardActionButton(
+                                  icon: const Icon(
+                                    Icons.person,
+                                    color: Colors.white,
+                                    size: 20,
+                                  ),
+                                  label: 'Veterinário',
+                                  onPress: () => _showVeterinarianModal(
+                                      activation.veterinarian),
+                                ),
+                                CardActionButton(
+                                  icon: const Icon(
+                                    Icons.pets,
+                                    color: Colors.white,
+                                    size: 20,
+                                  ),
+                                  label: 'Animais',
+                                  onPress: _showAnimalsModal,
+                                ),
+                                CardActionButton(
+                                  icon: const Icon(
+                                    Icons.build,
+                                    color: Colors.white,
+                                    size: 20,
+                                  ),
+                                  label: 'Serviços',
+                                  onPress: _showServicesModal,
+                                ),
+                              ],
+                              child: ActivationCard(
+                                activationDate: activation.activationDate,
+                                activationId: activation.activationId,
+                                scheduledDate: activation.scheduledDate,
+                                status: activation.status,
+                                type: activation.type,
                               ),
-                              label: 'Veterinário',
-                              onPress: () {},
-                            ),
-                            CardActionButton(
-                              icon: const Icon(
-                                Icons.pets,
-                                color: Colors.white,
-                                size: 20,
-                              ),
-                              label: 'Animais',
-                              onPress: () {},
-                            ),
-                            CardActionButton(
-                              icon: const Icon(
-                                Icons.build,
-                                color: Colors.white,
-                                size: 20,
-                              ),
-                              label: 'Serviços',
-                              onPress: () {},
-                            ),
-                          ],
-                          child: ActivationCard(
-                            activationDate: activation.activationDate,
-                            activationId: activation.activationId,
-                            scheduledDate: activation.scheduledDate,
-                            status: activation.status,
-                            type: activation.type,
-                          ),
-                        ));
+                            ));
                       },
                     ),
                   ),
                 ],
               );
             } else if (snapshot.hasError) {
-              return Center(
+              return const Center(
                 child: Text('Erro ao carregar acionamentos!'),
               );
             } else {
-              return Center(
+              return const Center(
                 child: Text('Nenhum acionamento encontrado'),
               );
             }
