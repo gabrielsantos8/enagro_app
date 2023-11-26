bbaea2c2217cfcc5ab52dcd53daa6823aab4ad28
gabriel.santos <gabrieldossantossilva2108@gmail.com>
2023-11-26 15:01:36 -0300

    FEAT: validacao endereços

--- lib/models/user_address.dart
+++ lib/models/user_address.dart
@@ -5,9 +5,11 @@ class UserAddress {
   final String complement;
   final City city;
   final int userId;
+  final bool? isNotDeletable;
 
   UserAddress(
-      {required this.userAddressId,
+      {this.isNotDeletable,
+      required this.userAddressId,
       required this.complement,
       required this.city,
       required this.userId});
@@ -16,6 +18,7 @@ class UserAddress {
     return UserAddress(
         userAddressId: usr['id'] ?? 0,
         complement: usr['complement'] ?? '',
+        isNotDeletable: usr['is_not_deletable'] ?? false,
         city: City.getCity(usr['city_id'] ?? 0, usr['city'] ?? '',
             usr['uf'] ?? '', usr['ibge'] ?? 0),
         userId: usr['user_id'] ?? 0);
